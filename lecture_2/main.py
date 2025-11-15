235d05e16f3b18269d2166e93821d83acb0af207
Alexey <lesan3834@gmail.com>
2025-11-15 14:50:48 +0300

    feat: add dict of hobbies and completed the fourth part

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -7,15 +7,38 @@ def generate_profile(age):
         return ('Adult')
 
 
-user_name = input("Enter your name:")
+user_name = input("Enter your full name:")
 birth_year_str = input("Enter your birth year:")
 birth_year = int(birth_year_str)
 current_age = 2025 - birth_year
 hobbies = []
 while True:
-    hobby = input("Enter а favorite hobby ог type 'stop' to finish: ")
+    hobby = input("Enter a favorite hobby оr type 'stop' to finish: ")
     if hobby.strip().lower() == "stop":
         break
     hobbies.append(hobby)
-print(hobbies)
-generate_profile(5)
+life_stage = generate_profile(current_age)
+user_profile = {"name": user_name,
+                "age": current_age,
+                "stage": life_stage,
+                "hobbies": hobbies}
+profile_summary = """---
+Profile Summary:"""
+for key, value in user_profile.items():
+    match key:
+        case "name":
+            profile_summary += f"\nName: {value}\n"
+        case "age":
+            profile_summary += f"Age: {value}\n"
+        case "stage":
+            profile_summary += f"Life Stage: {value}\n"
+        case "hobbies":
+            num_hobby = len(value)
+            if num_hobby == 0:
+                profile_summary += "You didn't mention any hobbies.\n"
+            else:
+                profile_summary += f"Favorite Hobbies ({num_hobby}):\n"
+                for h in value:
+                    profile_summary += f"- {h}\n"
+profile_summary += "---"
+print(profile_summary)
